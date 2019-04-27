12c22b5c196294774867caf2029227a3a84dd8e9
Mayank <Mayank>
2019-04-28 03:21:42 +0530

    Profile Init

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!-- <!doctype html> -->
 <html lang="en">
 
 <head>
@@ -8,7 +8,7 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 
   <title>IITG - Mess Rating System</title>
@@ -18,39 +18,39 @@
 
 
   <div class="container-fluid">
-          <div class="row">
-            <div class="col-lg-2"></div>
-            <div class="col align-self-center">
+    <div class="row">
+      <div class="col-lg-2"></div>
+      <div class="col align-self-center">
 
 
 
-  <div class="card">
-    <h5 class="card-header">Enter IITG credentials</h5>
-    <div class="card-body">
-      <p><input class="form-control" placeholder="Username" id="chat-head-input" type="text" size="100"/></p>
-      <p><input class="form-control" placeholder="Password" id="chat-message-input" type="password" size="100"/></p>
-      <input class="btn btn-primary" id="chat-message-submit" type="button" value="Login"/>
+        <div class="card">
+          <h5 class="card-header">Enter IITG credentials</h5>
+          <div class="card-body">
+            <p><input class="form-control" placeholder="Username" id="chat-head-input" type="text" size="100"/></p>
+            <p><input class="form-control" placeholder="Password" id="chat-message-input" type="password" size="100"/></p>
+            <input class="btn btn-primary" id="chat-message-submit" type="button" value="Login"/>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-2"></div>
     </div>
   </div>
-</div>
-<div class="col-lg-2"></div>
-</div>
-</div>
   <!-- {% block your_content %}
   {% endblock %} -->
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+  crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
-    <!-- {% block scripts %}
-    {% endblock %} -->
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+  crossorigin="anonymous"></script>
+  <!-- {% block scripts %}
+  {% endblock %} -->
 </body>
 
 </html>
