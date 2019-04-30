06b16b3df7801a2e732be1940afd0d4b556b45ce
Udbhav Chugh <udbha170123055@iitg.ac.in>
2019-04-30 23:40:31 +0530

    Images

--- home.php
+++ home.php
@@ -128,6 +128,69 @@ die("Connection failed: " . $conn->connect_error);
 </div>
 
 
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+                  <img src="images/small_mess.jpg" class="d-block w-100" alt="image">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>IITG Mess Feedback System</h5>
+                <p>The one stop solution to Mess Reports, Feedback, Change Mess and Admin support</p>
+              </div>
+    </div>
+    <div class="carousel-item">
+               <img src="images/droppedImage.jpg" class="d-block w-100" alt="image">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>IITG Mess Feedback System</h5>
+                <p>The one stop solution to Mess Reports, Feedback, Change Mess and Admin support</p>
+              </div>
+    </div>
+
+
+
+    <div class="carousel-item">
+               <img src="images/IIT-guwahati.jpg" class="d-block w-100" alt="image">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>IITG Mess Feedback System</h5>
+                <p>The one stop solution to Mess Reports, Feedback, Change Mess and Admin support</p>
+              </div>
+    </div>
+
+    <div class="carousel-item">
+               <img src="images/06.jpg" class="d-block w-100" alt="image">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>IITG Mess Feedback System</h5>
+                <p>The one stop solution to Mess Reports, Feedback, Change Mess and Admin support</p>
+              </div>
+    </div>
+
+        <div class="carousel-item">
+               <img src="images/slide2.jpg" class="d-block w-100" alt="image">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>IITG Mess Feedback System</h5>
+                <p>The one stop solution to Mess Reports, Feedback, Change Mess and Admin support</p>
+              </div>
+    </div>
+
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
   <div class="container-fluid">
           <div class="row">
             <div class="col-lg-2"></div>
